072db8f3396973fb1e1d6f5810751a738de510e8
juggerNOT4 <abhisht9000@gmail.com>
2021-07-26 09:03:36 +0530

    This is the assignment for week 2

---
+++ Temp not version control/cachematrix.R
@@ -0,0 +1,33 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+        m<-NULL
+        set<- function(y){
+        x<<-y
+        m<<-NULL
+          }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- funtion() m
+        
+        list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+          message("Getting cached data")
+          return(m)
+            }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setsolve(m)
+        m
+}
